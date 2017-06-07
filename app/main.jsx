8e872d1cbe1aa569e3d46ee0d77b897bb6c9c961
Josh White <whitejosh42@gmail.com>
2017-06-07 13:05:57 -0500

    feat(products): /products onEnter loads all products from DB onto state

--- app/main.jsx
+++ app/main.jsx
@@ -8,51 +8,15 @@
 import 'babel-polyfill'
 
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
-import {connect, Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 
 import store from './store'
-import Jokes from './components/Jokes'
-import Login from './components/Login'
-import WhoAmI from './components/WhoAmI'
-import NotFound from './components/NotFound'
-import AllProducts from './components/AllProducts/react/AllProductsContainer'
-import ManageCart from './components/ManageCart/react/ManageCart'
-import Checkout from './components/CheckoutCart/react/Checkout'
-import { retrieveItems } from './components/ManageCart/redux/action-creators'
-
-import { fetchAllProducts } from './components/AllProducts/reducers/products'
-
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth }),
-  // (dispatch) => ({
-  //   loadProducts: () => {
-  //     dispatch(fetchAllProducts())
-  //   }
-  // })
-)(
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav>
-      {children}
-    </div>
-)
+import Routes from './Routes'
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
-        <Route path="/products" component={AllProducts} />
-        <Route path="/cart" component={ManageCart}/>
-        <Route path="/checkout" component={Checkout} />
-      </Route>
-      <Route path='*' component={NotFound} />
-    </Router>
+    <Routes />
   </Provider>,
   document.getElementById('main')
 )
