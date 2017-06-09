874cd37ca3fa3b5851679930df42f170a8d7f9a9
Josh White <whitejosh42@gmail.com>
2017-06-09 15:13:55 -0500

    test(cart): added cart test skeleton

--- server/cart.js
+++ server/cart.js
@@ -8,11 +8,7 @@ module.exports = require('express').Router()
 
   .get('/:id/products',
     (req, res, next) =>
-      Cart.findOne({
-        where: {
-          id: req.params.id
-        }
-      })
+      Cart.findById(req.params.id)
       .then(currentCart => {
         if (!currentCart) {
           res.json('Your cart is empty!')
@@ -24,24 +20,17 @@ module.exports = require('express').Router()
   )
 
   .put('/:id/products',
-      (req, res, next) =>
-        Cart.findOne({where: {user_id: req.params.id}})
-        .then(cart => {
-          if (!cart) {
-            return Cart.create({user_id: this.id})
-            .then(newCart => InCart.create({cart_id: newCart.id, product_id: req.body.id}))
-          }
-          return InCart.findOne({
-            where: {
-              cart_id: cart.id,
-              product_id: req.body.id
-            }
-          })
-          .then(foundIncart => {
-            if (!foundIncart) return InCart.create({cart_id: req.params.id, product_id: req.body.id})
-            const newQuanity = foundIncart.quantity + req.body.amount
-            return foundIncart.update({quantity: newQuanity})
-            .then(() => res.status(204).json('Updated cart.'))
-          })
-        })
-        .catch(next))
+    (req, res, next) =>
+      Cart.findById(req.params.id)
+      .then(cart => {
+        if (!cart) {
+          console.log(req.params.id)
+          console.log(req.body.product)
+          res.sendStatus(404)
+        } else {
+          cart.addProduct(req.body.item).then( add => console.log(add))
+          res.sendStatus(200)
+        }
+      })
+      .catch(next)
+  )
--- server/cart.test.js
+++ server/cart.test.js
@@ -15,31 +15,43 @@ const Products = db.model('products')
 const request = require('supertest')
 const app = require('./start')
 
-/* global describe it beforeEach afterEach before */
+/* global describe xit it xdescribe beforeEach afterEach before */
 
-describe.only('Cart API:', () => {
+xdescribe('Cart API:', () => {
   var server, user, cart, product
 
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
-  describe('returns items in a users cart:', () => {
+  beforeEach(() => User.create({ email: 'god@example.com', name: 'So many names', password: '1234' })
+    .then(u => user = u)
+  )
+  beforeEach(() => Cart.create({ user_id: user.id })
+    .then( c => cart = c)
+    .then(() => Products.create({name: 'Christmas Tree', price: 100, type: 'Home'}))
+    .then(p => product = p)
+  )
 
-    beforeEach(() => User.create({ email: 'god@example.com', name: 'So many names', password: '1234' })
-      .then(u => user = u)
-    )
-    beforeEach(() => Cart.create({ user_id: user.id })
-      .then( c => cart = c)
-      .then(() => Products.create({name: 'Christmas Tree', price: 100, type: 'Home'}))
-      .then(p => product = p)
-      .then(() => cart.addProduct(product))
-    )
+  describe('returns items in a users cart:', () => {
+    beforeEach(() => cart.addProduct(product))
 
     it('returns items if cart exists:', (done) => {
       request(app)
         .get('/api/cart/1/products')
-        .expect((res) => {
+        .expect( res => {
           expect(res.body.products).to.have.length(1)
+        })
+        .expect(200, done)
+    })
+  })
+
+  xdescribe('adds an item to a users cart:', () => {
+
+    it('adds one item to the cart:', (done) => {
+      request(app)
+        .put('/api/cart/1/products', product)
+        .expect( res => {
+          console.log('---')
         })
         .expect(200, done)
     })
