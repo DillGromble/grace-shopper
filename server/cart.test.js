c282996c4af8d43477ea848c870aacf46cc599a3
Josh White <whitejosh42@gmail.com>
2017-06-10 11:55:00 -0500

    feat(cart): basic functionality and tests working

--- server/cart.test.js
+++ server/cart.test.js
@@ -13,47 +13,60 @@ const User = db.model('users')
 const Products = db.model('products')
 
 const request = require('supertest')
-const app = require('./start')
+const app = request(require('./start'))
 
 /* global describe xit it xdescribe beforeEach afterEach before */
 
-xdescribe('Cart API:', () => {
+describe.only('Cart API:', () => {
   var server, user, cart, product
 
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
   beforeEach(() => User.create({ email: 'god@example.com', name: 'So many names', password: '1234' })
-    .then(u => user = u)
-  )
-  beforeEach(() => Cart.create({ user_id: user.id })
+    .then( u => user = u)
+    .then( () => Cart.create({ user_id: user.id }))
     .then( c => cart = c)
-    .then(() => Products.create({name: 'Christmas Tree', price: 100, type: 'Home'}))
-    .then(p => product = p)
+    .then( () => Products.create({name: 'Christmas Tree', price: 100, type: 'Home'}))
+    .then( p => product = p)
   )
+
 
   describe('returns items in a users cart:', () => {
     beforeEach(() => cart.addProduct(product))
 
     it('returns items if cart exists:', (done) => {
-      request(app)
-        .get('/api/cart/1/products')
+      app
+        .get(`/api/cart/${cart.id}/products`)
         .expect( res => {
           expect(res.body.products).to.have.length(1)
         })
         .expect(200, done)
     })
+
+    it('fails if cart doesn\'t exist', (done) => {
+      app
+        .get(`/api/cart/000/products`)
+        .send(product)
+        .expect(404, done)
+    })
   })
 
-  xdescribe('adds an item to a users cart:', () => {
+
+  describe('adds an item to a users cart:', () => {
 
     it('adds one item to the cart:', (done) => {
-      request(app)
-        .put('/api/cart/1/products', product)
-        .expect( res => {
-          console.log('---')
-        })
+      app
+        .put(`/api/cart/${cart.id}/products`)
+        .send(product)
         .expect(200, done)
+    })
+
+    it('fails if cart doesn\'t exist', (done) => {
+      app
+        .put(`/api/cart/000/products`)
+        .send(product)
+        .expect(404, done)
     })
   })
 })
