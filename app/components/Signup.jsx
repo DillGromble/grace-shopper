6d6f79d31b14001e8e1904ca6cc397b916398675
aperez25 <alice.perez25@gmail.com>
2017-06-12 11:14:20 -0700

    feat(signup): started OAuth integration - still need to sync db and state

--- app/components/Signup.jsx
+++ app/components/Signup.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export const Signup = ({ signup }) => (
+  <div>
+    <div className="buffer oauth">
+    <p>
+      <a
+        href="api/auth/login/google"
+        className="btn btn-social btn-google">
+        <i className="fa fa-google" />
+        <span>Sign up with Google</span>
+      </a>
+    </p>
+    <p>
+      <a
+        href="api/auth/login/facebook"
+        className="btn btn-social btn-facebook">
+        <i className="fa fa-facebook" />
+        <span>Sign up with Facebook</span>
+      </a>
+    </p>
+  </div>
   <form onSubmit={evt => {
     evt.preventDefault()
     signup(evt.target.name.value, evt.target.email.value, evt.target.password.value)
@@ -12,6 +31,7 @@ export const Signup = ({ signup }) => (
     <input type="submit" value="Signup" />
     <p> Already have an account? <Link to='/login'>Login here!</Link></p>
   </form>
+</div>
 )
 
 import {signup} from 'APP/app/reducers/auth'
