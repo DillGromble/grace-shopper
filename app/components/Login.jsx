27c86eecacd9dfa5ecbd81dc3cd497c7f079ebb2
Jen Tam <jennifer.k.tam@gmail.com>
2017-06-13 23:53:01 -0700

    feat(design): updated products, product, header, and footer

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const Login = ({ login }) => (
   <div>
-    <h2 className="login-h2">
-      Sign In
-   </h2>
 
     <form className="loginForm" onSubmit={evt => {
       evt.preventDefault()
       login(evt.target.username.value, evt.target.password.value)
     } }>
+      <h2 className="login-h2">
+        Sign In
+      </h2>
       <input name="username" placeholder="email"/>
       <input name="password" type="password" placeholder="password"/>
       <input type="submit" value="Login" />
